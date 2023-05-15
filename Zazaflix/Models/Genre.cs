5d9ea07306e72dab473f2d2774357cc9f965dc66
Arthur <arthur.barrabonita@gmail.com>
2023-05-15 09:36:54 -0300

    Mudanças no Genre e Movie

--- Zazaflix/Models/Genre.cs
+++ Zazaflix/Models/Genre.cs
@@ -1,12 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
-namespace Zazaflix.Models
-{
-    public class Genre
-    {
-        
-    }
+namespace Zazaflix.Models;
+
+ public class Genre
+ {
+     [Key] //Define a propriedade como Chave Primária
+    public byte Id { get; set; }
+
+
+    [Required] //Requirido - Not Null; Validação
+    [StringLength(30, ErrorMessage ="Porfavor, apenas 30 caracteres")]
+    public string Name { get; set; }
+
+
 }
